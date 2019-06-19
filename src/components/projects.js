274bc03499d2b1ce63f28ce0c3a50811cf257b21
ace_shah <skchandon@skchandon.com>
2019-06-19 14:03:05 -0400

    Projects.js complete

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,16 +10,30 @@ class Project extends Component {
     toggleCategories(){
         if(this.state.activeTab===0){
             return(
+                <div className="projects-grid">
+                    {/*project1 */}
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color:'#fff', height:'176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover'}}> React Project #1</CardTitle>
                     <CardText>React Portfolio </CardText>
                     <CardActions border>
-                        <Button colored>GitHub</Button>
+                        <a href="https://github.com/Fjohora9333/React-app-fatimaportfolio"><Button colored>GitHub</Button></a>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                         <IconButton name='share'/>
                     </CardMenu>
                 </Card>
+                {/*project2 */}
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color:'#fff', height:'176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover'}}> React Project #2</CardTitle>
+                    <CardText>React Portfolio </CardText>
+                    <CardActions border>
+                        <a href="https://github.com/Fjohora9333/React-app-fatimaportfolio"><Button colored>GitHub</Button></a>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share'/>
+                    </CardMenu>
+                </Card>
+                </div>
             )
     
         }else if(this. state.activeTab===1){
@@ -45,16 +59,15 @@ class Project extends Component {
                     <Tab>C#</Tab>
                     <Tab>PHP</Tab>
                 </Tabs>
-                <section className="projects-grid">
-                    <Grid className="projects-grid">
-                        <Cell col={12}>
-                            <div className="content">
-                            {this.toggleCategories()}
-                            </div>
-                        </Cell>
-                    </Grid>
-                </section>
-
+                
+                <Grid >
+                    <Cell col={12}>
+                        <div className="content">
+                        {this.toggleCategories()}
+                        </div>
+                    </Cell>
+                </Grid>
+               
             </div>
         )
     }
