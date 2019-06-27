96ca7d3b5ac868c11c17f3d90e52c291d5114455
ace_shah <skchandon@skchandon.com>
2019-06-27 11:15:40 -0400

    Experience

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 import Education from './education';
+import Experience from './experience';
 
 class Resume extends Component {
     render(){
@@ -28,10 +29,35 @@ class Resume extends Component {
                     </Cell>
                     <Cell className="resume-right-col" col={8}>
                         <h2>Education</h2>
-                        <Education startYear={2018} endYear={2019} schoolName="Conestoga College" schoolDescription="Computer Applications Development" courseItem1="Software Development - C#, WPF, MVVM, Object oriented programming, Visual Studio 2017" courseItem2="Database Development - MSSQL, database schema creation, stored procedure, trigger, normalization" 
-                        courseItem3="Web design - PHP, JQuery, AJAX, Bootstrap, HTML, CSS, WordPress" courseItem4="System Analysis and Design - SDLC, UML diagrams, Agile, Scrum" courseItem5="MS Web Technology - ASP.NET core, MVC, Annotation, Validation, Entity Framework" courseItem6="Mobile Application Development - Node.js, ReactJs, JavaScript, es6, FireBase, NoSQL, Heroku, GitHub"/>
-                        <Education startYear={2010} endYear={2014} schoolName="Khulna University of Engineering And Technology" schoolDescription="Civil Engineering" courseItem1="Numerical Analysis and Computer Programming" courseItem2=" Mathematics, Physics, Chemistry" courseItem3="Structural Analysis and Design" courseItem4="Environmental Engineering" courseItem5="Engineering Hydraulics" courseItem6="Geotechnical Engineering"/>
+                        <Education 
+                        startYear={2018} 
+                        endYear={2019} 
+                        schoolName="Conestoga College" schoolDescription="Computer Applications Development (Co-op), Ontario College Graduate Certificate" courseItem1="Software Development - C#, WPF, MVVM, Object oriented programming, Visual Studio 2017" courseItem2="Database Development - MSSQL, database schema creation, stored procedure, trigger, normalization" 
+                        courseItem3="Web design - PHP, JQuery, AJAX, Bootstrap, HTML, CSS, WordPress" 
+                        courseItem4="System Analysis and Design - SDLC, UML diagrams, Agile, Scrum" 
+                        courseItem5="MS Web Technology - ASP.NET core, MVC, Annotation, Validation, Entity Framework" courseItem6="Mobile Application Development - Node.js, ReactJs, JavaScript, es6, FireBase, NoSQL, Heroku, GitHub"/>
+                        <Education 
+                        startYear={2010} 
+                        endYear={2014} 
+                        schoolName="Khulna University of Engineering And Technology" 
+                        schoolDescription="Civil Engineering, Bachelor of Science" 
+                        courseItem1="Numerical Analysis and Computer Programming" 
+                        courseItem2=" Mathematics, Physics, Chemistry" courseItem3="Structural Analysis and Design" courseItem4="Environmental Engineering" courseItem5="Engineering Hydraulics" courseItem6="Geotechnical Engineering"/>
                         <hr style={{borderTop:'2px solid #e22947'}}/>
+                        <h2>Experience</h2>
+                        <Experience 
+                        startYear={2017} 
+                        endYear="Present" 
+                        jobName="Freelance Developer" 
+                        jobDescription="SQL database design, Built WordPress site, Drupal7 template development, Built C# WPF desktop Application
+                        "/>
+                        <Experience 
+                        startYear={2018} 
+                        endYear={2018}
+                        jobName="Cashier, Customer Service Representative" 
+                        jobDescription="Responded to customer inquiries in person as well as over the phone, Customer account management involving account adjustments, Health and safety training"/>
+                        <hr style={{borderTop:'2px solid #e22947'}}/>
+                        <h2>Skills</h2>
                     </Cell>
                 </Grid>
             </div>
